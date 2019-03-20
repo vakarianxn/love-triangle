3de8a102c243dce1bff1d7b78acefe4025b95308
xenia_b <x_balotnikava@flo.health>
2019-03-20 18:23:35 +0300

    feat: Add solution of the task to index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+preferences = [];
+
+module.exports = function getLoveTrianglesCount(preferences) {
+  const array_length = preferences.length;
+    let count = 0;
+    for (let i = 0; i < array_length; i++) {
+      let j = preferences[i];
+      let k = preferences[j-1];
+      let l = preferences[k-1];
+      if ((j != k) && (k != l) && ((i+1) == l)) {
+        count+=1;
+        preferences[i]='triangle';
+      };
+    }
+    return(count);
+  };
